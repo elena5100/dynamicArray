3dc60ab38d5c5bca0747d3d12ad19cf1b20c2afe
elena5100 <194572363+elena5100@users.noreply.github.com>
2025-04-14 03:21:01 -0700

    Added tests for adding one and multiple items to DynamicStringList

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -1,3 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class DynamicStringListTest {
-    
+
+    @Test
+    void testAddOneItem() {
+        DynamicStringList list = new DynamicStringList();
+        list.add("A");
+        assertEquals("A", list.get(0));
+    }
+
+    @Test
+    void testAddMultipleItems() {
+        DynamicStringList list = new DynamicStringList();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        assertEquals("C", list.get(2));
+    }
 }
