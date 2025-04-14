f50d37604554255037a1b1f746e89f435bf13f20
elena5100 <194572363+elena5100@users.noreply.github.com>
2025-04-14 03:30:28 -0700

    Added basic tests for add, get, set, and remove methods

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -18,4 +18,28 @@ public class DynamicStringListTest {
         list.add("C");
         assertEquals("C", list.get(2));
     }
+    @Test
+    void testSetItem() {
+        DynamicStringList list = new DynamicStringList();
+        list.add("A");
+        list.set(0, "Z");
+        assertEquals("Z", list.get(0));
+    }
+
+    @Test
+    void testGetItemAtIndex() {
+        DynamicStringList list = new DynamicStringList();
+        list.add("X");
+        assertEquals("X", list.get(0));
+    }
+    @Test
+    void testRemoveItem() {
+        DynamicStringList list = new DynamicStringList();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        String removed = list.remove(1);
+        assertEquals("B", removed);
+    }
+
 }
